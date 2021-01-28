6e8bb5ef96ec1e26d411578ae18df15ddbdcf24f
DESKTOP-MO9T27C\Windows <chalermsilp.thit@bumail.net>
2021-01-28 21:06:41 +0700

    Send Assignment Week1

---
+++ GI455_UnityProject_Week1/Assets/Scripts/inputData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inputData : MonoBehaviour
+{
+    public Text text;
+
+    public string[] data;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text = GetComponent<Text>();
+
+        //show data on board
+        text.text = data[0] + "\n" + data[1] + "\n" + data[2] + "\n" + data[3] + "\n" + data[4];
+    }
+}
---
+++ GI455_UnityProject_Week1/Assets/Scripts/inputFind.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class inputFind : MonoBehaviour
+{
+    public string input;
+    public Text text;
+    
+    inputData Data;
+
+    public void ReadAndInput(string s)
+    {
+        //check user input
+        input = s;
+        print(input);
+    }
+   
+    public void FindButton()
+    {
+        //get inputData script
+        Data = GameObject.FindObjectOfType<inputData>();
+        
+        //check user input with data board
+        for(int i = 0; i < Data.data.Length; i++)
+         {
+             if(Data.data[i] == input)
+             {
+                 text.text = "[ " + "<color=green>" + Data.data[i] + "</color>" + " ] is found.";
+                 break;
+             }
+             else
+             {
+                 text.text = "[ " + "<color=red>" + input + "</color>" + " ] is not found.";
+             }
+         }
+    }
+
+}
